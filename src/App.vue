dc6e004aada984d763d66b3be4a101d52edffb2b
DachiTs2 <dtsitsilashvili1@gmail.com>
2023-12-26 18:40:48 +0400

    hero

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <Navbar />
+  <Hero />
 </template>
 
 <script setup>
+import Hero from "./components/Hero.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full h-[450px] flex justify-center items-center">
+    <div class="w-5/6 h-full flex justify-between items-center">
+      <p class="text-black text-[64px] font-bold leading-[72px]">ბლოგი</p>
+      <div class="w-2/3 h-full flex justify-end items-end">
+        <img
+          src="../assets/space.png"
+          alt="Hero Image"
+          class="w-full h-full object-cover object-center"
+        />
+      </div>
+    </div>
+  </div>
+</template>
