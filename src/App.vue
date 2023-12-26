4f6daaa7d5c47c5eb5b06e735c8c0a4d7f24e1d8
DachiTs2 <124523353+DachiTs2@users.noreply.github.com>
2023-12-26 22:37:21 +0400

    blog container

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <Navbar />
   <Hero />
   <Categories />
+  <BlogContainer />
 </template>
 
 <script setup>
 import { onBeforeMount } from "vue";
 import Hero from "./components/Hero.vue";
+import BlogContainer from "./components/BlogsContainer.vue";
 import Navbar from "./components/Navbar.vue";
 import axios from "./helpers/axios";
 import Categories from "./components/Categories.vue";
---
+++ src/components/BlogsContainer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full h-auto flex justify-center items-center mt-16">
+    <div class="w-5/6 h-auto grid grid-cols-3 gap-x-8 gap-y-14">
+      <div class="col-span-1 h-[620px] bg-red-500"></div>
+    </div>
+  </div>
+</template>
