2d62af2e15fb773f5cb771db47edaf0a5ec58af3
DachiTs2 <124523353+DachiTs2@users.noreply.github.com>
2023-12-26 22:52:11 +0400

    blog card static

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="col-span-1 h-[620px] rounded-xl overflow-hidden flex flex-col justify-start items-start"
+  >
+    <div class="w-full h-80 rounded-xl">
+      <img
+        src="https://cdn.vox-cdn.com/thumbor/KA23i52Q1953XDw1_87hpmii6Jc=/0x0:2040x1360/768x768/filters:focal(1020x680:1021x681)/cdn.vox-cdn.com/uploads/chorus_asset/file/24931305/236791_Apple_iPhone_15_and_15_Plus_review_DSeifert_0008.jpg"
+        alt="Blog Image"
+        class="w-full h-full object-cover object-center"
+      />
+    </div>
+    <p class="text-black mt-6 font-medium">დაჩი წიწილაშვილი</p>
+    <p class="text-[#85858D] text-xs mt-2">19.12.2023</p>
+    <p class="text-black text-xl mt-4 font-medium">
+      EOMM-ის მრჩეველთა საბჭოს ნინო ეგაძე შეუერთდა
+    </p>
+    <div class="w-full h-7 space-x-4 flex justify-start items-center mt-4">
+      <div
+        class="w-auto px-2.5 py-1.5 bg-yellow-500/30 text-yellow-500 flex justify-center items-center rounded-3xl font-medium"
+      >
+        კატეგორია
+      </div>
+    </div>
+    <p class="text-[#404049] mt-4">
+      6 თვის შემდეგ ყველის ბრმა დეგუსტაციის დროც დადგა. მაქსიმალური
+      სიზუსტისთვის, ეს პროცესი...
+    </p>
+    <p class="text-[#5D37F3] text-sm mt-4 font-medium">სრულად ნახვა</p>
+  </div>
+</template>
--- src/components/BlogsContainer.vue
+++ src/components/BlogsContainer.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="w-full h-auto flex justify-center items-center mt-16">
     <div class="w-5/6 h-auto grid grid-cols-3 gap-x-8 gap-y-14">
-      <div class="col-span-1 h-[620px] bg-red-500"></div>
+      <BlogCard />
     </div>
   </div>
 </template>
+
+<script setup>
+import BlogCard from "./BlogCard.vue";
+</script>
